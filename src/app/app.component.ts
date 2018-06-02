76ba611b90c491af65498908a6697726253809f0
Badyl1g <wojtasgal1998@gmail.com>
2018-06-02 13:15:53 +0200

    Ensured environment security

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: environment.firebaseApiKey,
+      authDomain: environment.firebaseAuthDomain
+    })
+  }
+
+}
